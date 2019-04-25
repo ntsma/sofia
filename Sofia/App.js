810555c5bc355de3b3d0facbc84ba9d30dd1edbf
Eduardo <edusvieirap@gmail.com>
2019-04-25 16:08:51 -0300

    Renaming tabs

--- Sofia/App.js
+++ Sofia/App.js
@@ -1,26 +1,11 @@
 
 import React, {Component} from 'react';
+import {Platform, StyleSheet} from 'react-native';
+import { Container, Header, Content, Tab, Tabs } from 'native-base';
 
-import {
-  Platform,
-  StyleSheet
-} from 'react-native';
-
-import {
-  Container,
-  Header,
-  Content,
-  Tab,
-  Tabs,
-  Title,
-  Left,
-  Right,
-  Body
-} from 'native-base';
-
-import Tab1 from './tabOne';
-import Tab2 from './tabTwo';
-import Tab3 from './tabThree';
+import Home from './Home';
+import Questions from './Questions';
+import Settings from './Settings';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -34,23 +19,16 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Container>
-        <Header hasTabs>
-          <Left/>
-          <Body>
-            <Title>Sofia</Title>
-          </Body>
-          <Right />
-        </Header>
-
+        <Header hasTabs />
         <Tabs>
           <Tab heading="Home">
-            <Tab1 />
+            <Home />
           </Tab>
           <Tab heading="Perguntas">
-            <Tab2 />
+            <Questions />
           </Tab>
           <Tab heading="Configurações">
-            <Tab3 />
+            <Settings />
           </Tab>
         </Tabs>
       </Container>
--- Sofia/Settings.js
+++ Sofia/Settings.js
@@ -3,10 +3,6 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet} from 'react-native';
 import { Container, Header, Content, Tab, Tabs, Text } from 'native-base';
 
-import Tab1 from './tabOne';
-import Tab2 from './tabTwo';
-import Tab3 from './tabThree';
-
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -15,11 +11,11 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class tabTwo extends Component<Props> {
+export default class Settings extends Component<Props> {
   render() {
     return (
       <Container>
-        <Text>Dois</Text>
+        <Text>Settings</Text>
       </Container>
     );
   }
