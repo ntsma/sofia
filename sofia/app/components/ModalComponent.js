e2eb339e0f7e686ba1572c87fa12e702da9bfa09
sebastiaohns <sebastiaohns@gmail.com>
2019-11-18 11:23:39 -0300

    Updating SignUp flow

---
+++ sofia/app/components/ModalComponent.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default class ModalComponent extends Component {
+
+  render() {
+    return (
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.props.showConfirmation}>
+          <View style={styles.Container}>
+            <View style={styles.Modal}>
+              {this.props.content}
+              <TouchableOpacity
+                style={styles.Button}
+                onPress={this.props.onClose}>
+                <Text style={{ color: '#FFF' }}>Confirmar!</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  Container: {
+    width: '100%',
+    height: 'auto',
+    position: 'absolute',
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  Modal: {
+    width: '100%', 
+    height: '100%',
+    backgroundColor: '#f7f7f7',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 50,
+  },
+
+  Button: {
+    width: '80%',
+    height: 46,
+    backgroundColor: '#3c8dbc',
+    justifyContent:'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+
+})
