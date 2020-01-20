b601bc7c15cfb031e263f0065d14c66bf83c5980
Sebastião Henrique <sebastiaohns@gmail.com>
2020-01-20 13:59:22 -0300

    Configuring fb sdk

--- android/app/src/main/java/com/huufmasofia1/app/MainActivity.java
+++ android/app/src/main/java/com/huufmasofia1/app/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Intent;
 
 
 public class MainActivity extends ReactActivity {
@@ -12,6 +13,12 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
     @Override
     protected String getMainComponentName() {
         return "app";
