8a731f054e57348e56b4b6a93f553e7c92a75e42
VitorNunesCPT <vitornunescpt@gmail.com>
2023-09-29 10:48:00 -0300

    :alien: Update domain URL API

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://sofia.huufma.br/api'
+    baseURL: 'https://sofia.telessaude.ufma.br/api'
 });
 
 export default API;
+
